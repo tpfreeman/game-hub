a7b83ee28972124c7068ad7662b541544d0c06e3
Taylor <taylorpfreeman@gmail.com>
2024-02-02 00:52:27 +0100

    Get optimized images

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">Aside</GridItem>
+        <GridItem area="aside"></GridItem>
       </Show>
       <GameGrid />
     </Grid>
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,6 +11,7 @@ import {
 } from "@chakra-ui/react";
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -19,12 +20,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image
-        maxH={{ base: "200px", lg: "250px" }}
-        minH={{ base: "130px", lg: "150px" }}
-        objectFit="cover"
-        src={game.background_image}
-      />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Link>
           <Heading fontSize="2xl">{game.name}</Heading>
