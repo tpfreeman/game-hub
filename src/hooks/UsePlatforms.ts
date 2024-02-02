13607e171b51465ae872811519efaf1cb4f0ad32
Taylor <taylorpfreeman@gmail.com>
2024-02-02 13:23:10 +0100

    build platform selector

---
+++ src/hooks/UsePlatforms.ts
@@ -0,0 +1,11 @@
+import useData from "./useData"
+
+interface Platform {
+    id: number,
+    name: string,
+    slug: string
+}
+
+const UsePlatforms = () => useData<Platform>('/platforms/lists/parents')
+
+export default UsePlatforms;
