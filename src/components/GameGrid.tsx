dc07381816189e076536026819bd378bca790929
Taylor <taylorpfreeman@gmail.com>
2024-02-02 11:40:05 +0100

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,9 +12,9 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
-        padding="10px"
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
+        spacing={3}
+        padding={"10px"}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,6 @@
+import { HStack, Image, Link, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
@@ -6,11 +8,22 @@ const GenreList = () => {
   console.log(data);
 
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY={1}>
+          <HStack>
+            <Image
+              boxSize="40px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg" marginLeft={4}>
+              {genre.name}
+            </Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
